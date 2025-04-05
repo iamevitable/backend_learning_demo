10920ddbde42f506170a42a6e72710483c41eedb
han <2907880087@qq.com>
2025-04-05 19:12:59 +0800

    lru缓存添加注释

--- src/main/java/demo/LRUCache.java
+++ src/main/java/demo/LRUCache.java
@@ -3,9 +3,9 @@ package demo;
 import jdk.jshell.spi.SPIResolutionException;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class LRUCache {
+    //内部存储的结构
     private static class Node{
         int key;
         int value;
@@ -16,18 +16,30 @@ public class LRUCache {
             this.value = value;
         }
     }
+
+    //队列容量
     private final int capacity;
+
+    //虚拟头节点
     private final Node dummy = new Node(0, 0);
+
+    //内部存储的hash表，表示key和node节点之间的关系
     private final HashMap<Integer, Node> keyToNode = new HashMap<>();
+
+    //构造函数
     public LRUCache(int capacity) {
         this.capacity = capacity;
         dummy.next = dummy;
         dummy.prev = dummy;
     }
+
+    //获得节点的值，存在返回值，不存在返回-1
     public int get(int key) {
         Node node = getNode(key);
         return node != null ? node.value : -1;
     }
+
+    //加入节点
     public void put(int key, int value) {
         Node node = getNode(key);
         if (node != null) {
@@ -43,6 +55,8 @@ public class LRUCache {
             remove(backNode);
         }
     }
+
+    //获取节点，存在返回节点，不存在返回null
     private Node getNode(int key) {
         if (!keyToNode.containsKey(key)) {
             return null;
@@ -52,10 +66,14 @@ public class LRUCache {
         pushFront(node);
         return node;
     }
+
+    //移除节点，既是双向链表移除节点的做法
     private void remove(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
+
+    //将节点放到双向链表的开始，通过虚拟头节点来实现
     private void pushFront(Node node) {
         node.prev = dummy;
         node.next = dummy.next;
