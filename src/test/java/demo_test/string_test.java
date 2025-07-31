241ecb029e9c4f2a4a5fa67da855e7b4fbcfa100
han <2907880087@qq.com>
2025-07-31 23:36:16 +0800

    string的测试 2

--- src/test/java/demo_test/string_test.java
+++ src/test/java/demo_test/string_test.java
@@ -1,9 +1,18 @@
 package demo_test;
 
+import java.util.HashMap;
+
 public class string_test {
     public static void main(String[] args) {
         String a = new String("han");
         String b = new String("han");
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put(a, 1);
+        System.out.println(hashMap.get("han"));
+        hashMap.put("han", 2);
+        System.out.println(hashMap.get("han"));
+        System.out.println(a);
+        System.out.println(b);
         System.out.println(a == b);
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
