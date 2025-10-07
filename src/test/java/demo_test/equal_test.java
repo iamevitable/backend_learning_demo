9a5bcf8a3143c2d6c2afd7b2abde42269a6a4556
iamevitable <2907880087@qq.com>
2025-10-07 15:31:59 +0800

    包装类装箱和缓冲池的实验

---
+++ src/test/java/demo_test/equal_test.java
@@ -0,0 +1,20 @@
+package demo_test;
+
+/**
+ * @Desc * @param
+ * @return
+ * @date:
+ * @Author
+ */
+public class equal_test {
+    public static void main(String[] args) {
+        int a = 100;
+        Integer b = 100;
+        Integer d = 100;
+        Integer e = 1000;
+        Integer f = 1000;
+        System.out.println(a == b);
+        System.out.println(b == d);
+        System.out.println(e == f);
+    }
+}
